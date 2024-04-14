c894f574d58e0250ea5ef43e04d7d95f44a612e6
jyoti012 <jyoti.suvarna.code@gmail.com>
2024-04-15 00:15:55 +0100

    Delete old files

--- frontend-app/src/config.js
+++
@@ -1,9 +0,0 @@
-// update api url here
-const API_URLS = {
-  getAllShows: process.env.REACT_APP_API_URL_GET_ALL_SHOWS,
-  getAllGenres: process.env.REACT_APP_API_URL_GET_ALL_GENRES,
-  getShowDates: process.env.REACT_APP_API_URL_GET_SHOW_DATES,
-  sendBooking: '',
-};
-
-export default API_URLS;
