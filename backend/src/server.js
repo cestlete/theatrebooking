7432e4247db92b9d322f08c3e70622504254d00b
Karl Mulraney <116092530+Kpmul@users.noreply.github.com>
2024-04-15 08:27:50 +0100

    Update server.js
    Updated comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,27 +6,30 @@
 *  Enables CORS, parses JSON data
 *  Connects to MongoDB, and starts the server on the specified port.
 */
-
+// Load environment variables from a .env file
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const showRoutes = require('./routes/showRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
+// Import libraries
+const express = require('express'); // HTTP requests 
+const cors = require('cors'); // Cross origin requests
+// Import route handlers
+const showRoutes = require('./routes/showRoutes'); // Shows
+const bookingRoutes = require('./routes/bookingRoutes'); // Bookings
+// Import utility to connect to database
 const { connectToDB } = require('./utils/db');
-connectToDB();
+connectToDB(); // Establish connection
 
 const app = express();
 
-// enable CORS
+// enable CORS middleware
 app.use(cors());
 // enable req.body JSON data
 app.use(express.json());
-
+// Use .env port default to 8000
 const PORT = process.env.PORT || 8000;
-
+// start server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
-
-app.use('/api', showRoutes);
-app.use('/api', bookingRoutes);
+// API routes
+app.use('/api', showRoutes); // Mount showRoutes under '/api' path
+app.use('/api', bookingRoutes); // Mount bookingRoutes under '/api' path
