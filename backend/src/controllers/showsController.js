8037f9065db7199accdcc70837f6f8c05d690293
Karl Mulraney <116092530+Kpmul@users.noreply.github.com>
2024-04-15 08:49:00 +0100

    Updated showsController.js

--- backend/src/controllers/showsController.js
+++ backend/src/controllers/showsController.js
@@ -15,7 +15,8 @@ const { getGenres, getDates } = require('../utils/db');
  */
 exports.getShows = async (req, res) => {
   try {
-    const shows = await Show.find({});
+    const shows = await Show.find({}); // Retrieve all shows from the database
+     // Calculate total remaining tickets for each show
     const showsWithSession = shows.map(show => {
       let totalRemain = 0;
       show.session.forEach(session => {
@@ -26,9 +27,10 @@ exports.getShows = async (req, res) => {
       return {
         ...show._doc,
         hasAvailability: totalRemain > 0
+         // set hasAvailability depending on remaining tickets
       };
     });
-    res.json(showsWithSession);
+    res.json(showsWithSession); // 
   } catch (error) {
     console.error('Error fetching show information:', error);
     res.status(500).json({ error: 'Internal Server Error' });
