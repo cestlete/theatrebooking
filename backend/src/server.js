2c6f741117fa1788fdbacda5f17b80f68cb81f2b
C'est l'été <hmq897@gmail.com>
2024-04-15 22:14:01 +0100

    Optimize comment readability

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,28 +8,40 @@
 */
 // Load environment variables from a .env file
 require('dotenv').config();
-// Import libraries
-const express = require('express'); // HTTP requests 
-const cors = require('cors'); // Cross origin requests
-// Import route handlers
-const showRoutes = require('./routes/showRoutes'); // Shows
-const bookingRoutes = require('./routes/bookingRoutes'); // Bookings
-// Import utility to connect to database
-const { connectToDB } = require('./utils/db');
-connectToDB(); // Establish connection
 
+// Import libraries
+// - HTTP requests 
+const express = require('express'); 
+// - Cross origin requests
+const cors = require('cors'); 
+
+// Import Route Handlers:
+// - Shows
+const showRoutes = require('./routes/showRoutes');
+// - Bookings
+const bookingRoutes = require('./routes/bookingRoutes'); 
+
+// Establish Database Connection:
+const { connectToDB } = require('./utils/db');
+connectToDB();
+
+// Create Express Application Instance
 const app = express();
 
-// enable CORS middleware
+// Enable CORS middleware
 app.use(cors());
-// enable req.body JSON data
+// Enable req.body JSON data
 app.use(express.json());
+
 // Use .env port default to 8000
 const PORT = process.env.PORT || 8000;
-// start server
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
 // API routes
-app.use('/api', showRoutes); // Mount showRoutes under '/api' path
-app.use('/api', bookingRoutes); // Mount bookingRoutes under '/api' path
+// - Mount showRoutes under '/api' path
+app.use('/api', showRoutes); 
+// - Mount bookingRoutes under '/api' path
+app.use('/api', bookingRoutes); 
