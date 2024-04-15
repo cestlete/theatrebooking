b51974b4f9327cd2567aa7bba6033107ff389cbc
C'est l'été <hmq897@gmail.com>
2024-04-15 22:14:01 +0100

    Optimize comment readability

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,22 +13,40 @@ const swaggerDefinition = require('./swagger/swaggerDefinition');
 
 connectToDB();
 
+// Import libraries
+// - HTTP requests 
+const express = require('express');
+// - Cross origin requests
+const cors = require('cors');
+
+// Import Route Handlers:
+// - Shows
+const showRoutes = require('./routes/showRoutes');
+// - Bookings
+const bookingRoutes = require('./routes/bookingRoutes');
+
+// Establish Database Connection:
+const { connectToDB } = require('./utils/db');
+connectToDB();
+
+// Create Express Application Instance
 const app = express();
 
-// Enable CORS
+// Enable CORS middleware
 app.use(cors());
 // Enable req.body JSON data
 app.use(express.json());
 // Enable Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { swaggerOptions: { ...swaggerDefinition } }));
-
-
+// Use .env port default to 8000
 const PORT = process.env.PORT || 8000;
-// start server
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
 
-// Define your API routes
+// API routes
+// - Mount showRoutes under '/api' path
 app.use('/api', showRoutes);
+// - Mount bookingRoutes under '/api' path
 app.use('/api', bookingRoutes);
