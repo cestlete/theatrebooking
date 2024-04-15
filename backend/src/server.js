643ad7aa6ccaf7aad36f4eaf6f0d0cf1e7acfa9b
Karl Mulraney <116092530+Kpmul@users.noreply.github.com>
2024-04-15 08:27:50 +0100

    Update server.js
    Updated comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const showRoutes = require('./routes/showRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
+// Import libraries
+const express = require('express'); // HTTP requests 
+const cors = require('cors'); // Cross origin requests
+// Import route handlers
+const showRoutes = require('./routes/showRoutes'); // Shows
+const bookingRoutes = require('./routes/bookingRoutes'); // Bookings
+// Import utility to connect to database
 const { connectToDB } = require('./utils/db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json'); // Assuming you have swagger.json file in the swagger folder
@@ -17,11 +20,11 @@ app.use(cors());
 // Enable req.body JSON data
 app.use(express.json());
 // Enable Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {swaggerOptions: {...swaggerDefinition} }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { swaggerOptions: { ...swaggerDefinition } }));
 
 
 const PORT = process.env.PORT || 8000;
-
+// start server
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
