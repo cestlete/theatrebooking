460af63362f4bac951f408b9da3421c975917472
Karl Mulraney <116092530+Kpmul@users.noreply.github.com>
2024-04-15 08:43:38 +0100

    Updated bookingsController.js

--- backend/src/controllers/bookingsController.js
+++ backend/src/controllers/bookingsController.js
@@ -13,14 +13,17 @@ const Show = require('../models/show');
  * @returns {Object} - JSON response with booking details or error message 
  */
 exports.getBookingDetails = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params; // Extract 'id' from request parameters
   try {
-    const booking = await Booking.findById(id);
+    const booking = await Booking.findById(id); // try retrieve booking from database
     if (!booking) {
+      // if no booking found
       return res.status(404).json({ error: 'Booking not found' });
     }
+    // otherwise, return booking as JSON
     res.json(booking);
   } catch (error) {
+    // Log any errors if they occour
     console.error('Error fetching booking:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -35,16 +38,19 @@ exports.getBookingDetails = async (req, res) => {
  * @returns {Object} - JSON response indicating success or failure of booking creation
  */
 exports.bookShow = async (req, res) => {
-  const { showId, date, price, ticketsBooked, bookingDetails } = req.body;
+  const { showId, date, price, ticketsBooked, bookingDetails } = req.body; // USe required fields from body
   try {
-    const show = await Show.findById(showId);
+    const show = await Show.findById(showId); // Retrieve show using 'showId'
     if (!show) return res.status(404).json({ error: 'Show not found' });
 
-    let remainUpdated = false;
+    let remainUpdated = false; // Track if ticket availability has been updated
     show.session.forEach(session => {
       if (session.date === date) {
+        // Check for specific date in 'session'
         session.ticketsAvailability.forEach(ticket => {
+          // Iterate over ticket types for that date
           if (ticket.price === parseFloat(price) && (ticket.remain - ticketsBooked >= 0)) {
+            // Update ticket count if there are tickets available 
             ticket.remain -= ticketsBooked;
             remainUpdated = true;
           }
@@ -53,9 +59,9 @@ exports.bookShow = async (req, res) => {
     });
 
     if (!remainUpdated) return res.status(400).json({ type: 'failure', error: 'Not enough tickets available or wrong price' });
-
-    await show.save();
-    const booking = await Booking.create(bookingDetails);
+     // if update failed return status 400
+    await show.save(); // Save updated show
+    const booking = await Booking.create(bookingDetails); // Create new booking record
     res.status(201).json({ type: 'success', message: 'Booking created successfully', booking });
   } catch (error) {
     console.error('Error making booking:', error);
